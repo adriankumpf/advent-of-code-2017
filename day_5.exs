993f8cf7d7c21696ad9c14e24515881c6189e23e
Adrian Kumpf <adrian@peaksandpies.com>
2017-12-07 23:41:38 +0100

    Refactor day 5

--- day_5.exs
+++ day_5.exs
@@ -3,20 +3,10 @@ defmodule Day5 do
     instructions
     |> String.trim()
     |> String.split("\n")
-    |> Enum.map(&to_int!/1)
-    |> to_map
-    |> jump(0, 0)
-  end
-
-  defp to_int!(str) do
-    {int, ""} = Integer.parse(str, 10)
-    int
-  end
-
-  defp to_map(list) do
-    list
+    |> Enum.map(&String.to_integer/1)
     |> Enum.with_index()
     |> Enum.into(%{}, fn {v, i} -> {i, v} end)
+    |> jump(0, 0)
   end
 
   defp jump(instructions, pos, count) do
@@ -24,8 +14,8 @@ defmodule Day5 do
            nil -> {:done, nil}
            cur -> {cur, cur + 1}
          end) do
-      {:done, _new_instructions} -> count
-      {move_by, new_instructions} -> jump(new_instructions, pos + move_by, count + 1)
+      {:done, _instr} -> count
+      {jump_by, instr} -> jump(instr, pos + jump_by, count + 1)
     end
   end
 end
